b5f246f30b5edcb1494d2163c12e9a4cd5e00d76
Jupri Naldi <juprinaldi023@gmail.com>
2025-06-15 01:49:04 +0700

    Edited

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-import Hapi from '@hapi/hapi';
-import predictRoutes from './routes/predict.js';
-import historyRoutes from './routes/history.js';
-import usersRoutes from './routes/users.js';
+import Hapi from "@hapi/hapi";
+import predictRoutes from "./routes/predict.js";
+import historyRoutes from "./routes/history.js";
+import usersRoutes from "./routes/users.js";
 
 const init = async () => {
   const server = Hapi.server({
-    port: process.env.PORT || 3001,
-    host: "localhost", // penting untuk Railway!
+    port: process.env.PORT || 3000,
+    host: "0.0.0.0",
     routes: {
       cors: {
-        origin: ["*"], // masih OK untuk testing, bisa diatur nanti
+        origin: ["*"], // Atau bisa diatur ke ['http://localhost:3000'] jika lebih aman
+        additionalHeaders: ["content-type"],
       },
     },
   });
-
 
   server.route([...usersRoutes, ...historyRoutes]);
   server.route(predictRoutes);
 
   await server.start();
-  console.log('🚀 Server running at:', server.info.uri);
+  console.log("🚀 Server running at:", server.info.uri);
 };
 
 init();
